3141e4aa4c754ff473cdf9b5f65b38e104493769
Utkarsh Singh <hsraktu007@gmail.com>
2026-02-10 12:37:34 +0530

    Added the basic files configurations and environment file to get started

---
+++ src/main/java/com/bharat/demo/BharatbankApplication.java
@@ -0,0 +1,13 @@
+package com.bharat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharatbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharatbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bharat/demo/BharatbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharatbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
